1fbcc6f92ec7fef38da48914ae33a92faeac51f3
gongping-cv <gxgongping@qq.com>
2026-08-10 14:35:28 +0800

    Update Mytrain_CMLNN_Jadeite_FlowAlignRGB_fixed_UCF101_for_iu.py

--- Mytrain_CMLNN_Jadeite_FlowAlignRGB_fixed_UCF101_for_iu.py
+++ Mytrain_CMLNN_Jadeite_FlowAlignRGB_fixed_UCF101_for_iu.py
@@ -160,7 +160,7 @@ class FixedLRExperimentConfig:
                  enable_stage2: bool = True,
 
                  # Data
-                 data_root: str = '../mytest06/dataset_npy_keepall/ucf101_processed',
+                 data_root: str = './dataset_npy_keepall/ucf101_processed',
                  clip_len: int = 32,
                  train_num_clips: int = 1,
                  test_num_clips: int = 3,
